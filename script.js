c16b0080a5e350030102c4bd9c81d5524b93e7a1
bridget462 <bridges.dev.create@gmail.com>
2020-10-10 12:20:50 +0900

    screen capture api tested on load

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
+// Prompt to select media stream, pass to video element and play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log(`whops!: ${error}`);
+  }
+}
+
+// on load
+selectMediaStream();
